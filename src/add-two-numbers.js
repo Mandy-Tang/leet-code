2f10a8e6870502b38152c82ca2aaf2c17d2d9129
Tang Man <189658646@qq.com>
2018-12-25 16:41:16 +0800

    [solve:optimization] two sum

--- src/add-two-numbers.js
+++ src/add-two-numbers.js
@@ -25,7 +25,7 @@ const addTwoNumbers = (l1, l2) => {
   let preNode = new ListNode(0);
   let result = preNode;
 
-  while (listNode1 || listNode2) {
+  while ((listNode1 && listNode2) || curry > 0) {
     const val1 = listNode1 ? listNode1.val : 0;
     const val2 = listNode2 ? listNode2.val : 0;
     const sum = val1 + val2 + curry;
@@ -38,8 +38,10 @@ const addTwoNumbers = (l1, l2) => {
     listNode2 = listNode2 ? listNode2.next : null;
   }
 
-  if (curry > 0) {
-    preNode.next = new ListNode(curry);
+  if (listNode1) {
+    preNode.next = listNode1;
+  } else if (listNode2) {
+    preNode.next = listNode2;
   }
 
   return result.next;
