89f355db8d69c1038cf269475320f28ff54cbe9c
Tang Man <189658646@qq.com>
2019-01-06 12:46:18 +0800

    [solve:optimization]: search insert position

--- src/search-insert-position.js
+++ src/search-insert-position.js
@@ -3,7 +3,28 @@
  * @param {number} target
  * @return {number}
  */
-const searchInsert = function(nums, target) {
+// const searchInsert = function(nums, target) {
+//   if (
+//     !nums ||
+//     nums.constructor.name !== "Array" ||
+//     typeof target !== "number"
+//   ) {
+//     return undefined;
+//   }
+
+//   const length = nums.length;
+
+//   for (let i = 0; i < length; i++) {
+//     if (nums[i] >= target) {
+//       return i;
+//     }
+//   }
+
+//   return length;
+// };
+
+// binary search and recursive
+const searchInsert = (nums, target) => {
   if (
     !nums ||
     nums.constructor.name !== "Array" ||
@@ -12,15 +33,23 @@ const searchInsert = function(nums, target) {
     return undefined;
   }
 
-  const length = nums.length;
-
-  for (let i = 0; i < length; i++) {
-    if (nums[i] >= target) {
-      return i;
-    }
+  if (nums.length === 0) {
+    return 0;
   }
 
-  return length;
+  if (nums.length === 1) {
+    return nums[0] >= target ? 0 : 1;
+  }
+
+  const length = nums.length;
+  const midIndex = Math.floor(length / 2);
+  if (nums[midIndex] > target) {
+    return searchInsert(nums.slice(0, midIndex), target);
+  } else if (nums[midIndex] < target) {
+    return midIndex + searchInsert(nums.slice(midIndex, length), target);
+  } else {
+    return midIndex;
+  }
 };
 
 export { searchInsert };
