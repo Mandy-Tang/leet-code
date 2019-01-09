ea9d1d722f2ec5fc0a9ea3652684a58b2d2550bf
Tang Man <189658646@qq.com>
2019-01-09 14:10:01 +0800

    [resolve] Remove Duplicates from sorted List

---
+++ src/83-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,33 @@
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const deleteDuplicates = function(head) {
+  if (!head) {
+    return null;
+  }
+
+  let preNode = new ListNode(head.val);
+  let resultNode = preNode;
+  let curNode = head.next;
+
+  while (curNode) {
+    if (curNode.val === preNode.val) {
+      curNode = curNode.next;
+    } else {
+      preNode.next = new ListNode(curNode.val);
+      preNode = preNode.next;
+      curNode = curNode.next;
+    }
+  }
+
+  return resultNode;
+};
+
+export { deleteDuplicates, ListNode };
---
+++ src/83-remove-duplicates-from-sorted-list.test.js
@@ -0,0 +1,27 @@
+import {
+  deleteDuplicates,
+  ListNode
+} from "./83-remove-duplicates-from-sorted-list";
+
+describe("Remove Duplicates from sorted List", () => {
+  it("should return null if there is no given list", () => {
+    expect(deleteDuplicates()).toBeNull();
+  });
+  it("should remove the dulicates from the given sorted list", () => {
+    const testNode = new ListNode(1);
+    const a = new ListNode(1);
+    const b = new ListNode(2);
+    const c = new ListNode(2);
+    const d = new ListNode(4);
+    testNode.next = a;
+    a.next = b;
+    b.next = c;
+    c.next = d;
+
+    const result = deleteDuplicates(testNode);
+    expect(result.val).toBe(1);
+    expect(result.next.val).toBe(2);
+    expect(result.next.next.val).toBe(4);
+    expect(result.next.next.next).toBeNull();
+  });
+});
