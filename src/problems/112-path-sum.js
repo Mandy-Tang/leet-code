c5b36f57516f34e60fb86a0618140e7ace2ccc60
Tang Man <189658646@qq.com>
2019-01-16 14:33:16 +0800

    [resolve:alternative] has path sum

--- src/problems/112-path-sum.js
+++ src/problems/112-path-sum.js
@@ -1,4 +1,5 @@
 /**
+ * recursive approach
  * @param {TreeNode} root
  * @param {number} sum
  * @return {boolean}
@@ -18,4 +19,35 @@ const hasPathSum = function(root, sum) {
   );
 };
 
+/**
+ * DFS iteration
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+// const hasPathSumUsingDFS = function(root, sum) {
+//   if (root === null) {
+//     return false;
+//   }
+
+//   const stack = [];
+//   stack.push({tree: root, sum: sum});
+//   while(stack.length !== 0) {
+//     const {tree: currentTree, sum} = stack.pop();
+//     if (!currentTree.left && !currentTree.right) {
+//       if (currentTree.val === sum) {
+//         return true;
+//       }
+//     } else {
+//       if (currentTree.left) {
+//         stack.push({tree: currentTree.left, sum: sum - currentTree.val})
+//       }
+//       if (currentTree.right) {
+//         stack.push({tree: currentTree.right, sum: sum - currentTree.val});
+//       }
+
+//     }
+//   }
+//   return false;
+// };
 export default hasPathSum;
