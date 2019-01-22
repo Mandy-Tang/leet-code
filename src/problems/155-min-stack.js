0eba3b4999c48bff7dd3002bf9e42c689f748a61
Tang Man <189658646@qq.com>
2019-01-22 13:10:17 +0800

    [solution] min stack using Class

--- src/problems/155-min-stack.js
+++ src/problems/155-min-stack.js
@@ -1,45 +1,70 @@
 /**
  * initialize your data structure here.
  */
-var MinStack = function() {
-  this.stack = [];
-};
+// var MinStack = function() {
+//   this.stack = [];
+// };
 
-/**
- * @param {number} x
- * @return {void}
- */
-MinStack.prototype.push = function(x) {
-  this.stack.push(x);
-};
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MinStack.prototype.push = function(x) {
+//   this.stack.push(x);
+// };
 
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-  this.stack.pop();
-};
+// /**
+//  * @return {void}
+//  */
+// MinStack.prototype.pop = function() {
+//   this.stack.pop();
+// };
 
-/**
- * @return {number}
- */
-MinStack.prototype.top = function() {
-  return this.stack[this.stack.length - 1];
-};
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.top = function() {
+//   return this.stack[this.stack.length - 1];
+// };
 
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function() {
-  if (this.stack.length === 0) {
-    return;
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.getMin = function() {
+//   if (this.stack.length === 0) {
+//     return;
+//   }
+//   const min = this.stack.reduce((acc, cur) => {
+//     return Math.min(acc, cur);
+//   }, Number.MAX_VALUE);
+
+//   return min;
+// };
+
+export class MinStack {
+  constructor() {
+    this.stack = [];
   }
-  const min = this.stack.reduce((acc, cur) => {
-    return Math.min(acc, cur);
-  }, Number.MAX_VALUE);
 
-  return min;
-};
+  push(number) {
+    this.stack.push(number);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  top() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin() {
+    const min = this.stack.reduce((acc, cur) => {
+      return Math.min(acc, cur);
+    }, Number.MAX_VALUE);
+    return min;
+  }
+}
 
 /**
  * Your MinStack object will be instantiated and called as such:
