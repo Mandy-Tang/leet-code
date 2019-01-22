3695c6d08c252bcb748be8fcf9b5f41084fa6483
Tang Man <189658646@qq.com>
2019-01-22 14:08:52 +0800

    [solution:optimization] intersection of two linked lists using pointer

--- src/problems/160-intersection-of-two-linked-lists.js
+++ src/problems/160-intersection-of-two-linked-lists.js
@@ -1,37 +1,50 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
+/** using hash Map
  * @param {ListNode} headA
  * @param {ListNode} headB
  * @return {ListNode}
  */
-const getIntersectionNode = function(headA, headB) {
+// const getIntersectionNode = function(headA, headB) {
+//   if (!headA || !headB) {
+//     return null;
+//   }
+
+//   const listSetA = new Set();
+//   let currentNode = headA;
+//   while (currentNode) {
+//     listSetA.add(currentNode);
+//     currentNode = currentNode.next;
+//   }
+
+//   currentNode = headB;
+//   while (currentNode) {
+//     if (listSetA.has(currentNode)) {
+//       return currentNode;
+//     }
+//     currentNode = currentNode.next;
+//   }
+
+//   return null;
+// };
+
+/** using two pointer
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = (headA, headB) => {
   if (!headA || !headB) {
     return null;
   }
 
-  const listSetA = new Set();
-  let currentNode = headA;
-  while (currentNode) {
-    listSetA.add(currentNode);
-    currentNode = currentNode.next;
+  let pointerA = headA;
+  let pointerB = headB;
+
+  while (pointerA !== pointerB) {
+    pointerA = pointerA ? pointerA.next : headB;
+    pointerB = pointerB ? pointerB.next : headA;
   }
 
-  currentNode = headB;
-  while (currentNode) {
-    if (listSetA.has(currentNode)) {
-      return currentNode;
-    }
-    currentNode = currentNode.next;
-  }
-
-  return null;
+  return pointerA ? pointerA : null;
 };
 
 export default getIntersectionNode;
