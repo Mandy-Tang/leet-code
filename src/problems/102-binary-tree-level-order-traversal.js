14f7dce538ac30ed6b0c03f8b18d6db7d7a7e657
Tang Man <189658646@qq.com>
2019-03-28 22:07:54 +0800

    [solution] binary-tree-level-order-traversal: dst

--- src/problems/102-binary-tree-level-order-traversal.js
+++ src/problems/102-binary-tree-level-order-traversal.js
@@ -1,36 +1,51 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// var levelOrder = function(root) {
+//   const result = [];
+//   if (!root) {
+//     return result;
+//   }
+
+//   let temp = [];
+//   temp.push(root);
+//   while (temp.length !== 0) {
+//     let currentLevel = temp.splice(0, temp.length);
+//     let currentLevelValue = [];
+//     currentLevel.forEach(treeNode => {
+//       currentLevelValue.push(treeNode.val);
+//       if (treeNode.left) {
+//         temp.push(treeNode.left);
+//       }
+//       if (treeNode.right) {
+//         temp.push(treeNode.right);
+//       }
+//     });
+//     result.push(currentLevelValue);
+//   }
+
+//   return result;
+// };
 var levelOrder = function(root) {
   const result = [];
   if (!root) {
     return result;
   }
 
-  let temp = [];
-  temp.push(root);
-  while (temp.length !== 0) {
-    let currentLevel = temp.splice(0, temp.length);
-    let currentLevelValue = [];
-    currentLevel.forEach(treeNode => {
-      currentLevelValue.push(treeNode.val);
-      if (treeNode.left) {
-        temp.push(treeNode.left);
-      }
-      if (treeNode.right) {
-        temp.push(treeNode.right);
-      }
-    });
-    result.push(currentLevelValue);
-  }
+  const dst = (tree, level, result) => {
+    if (!tree) {
+      return;
+    }
+    if (!result[level]) {
+      result[level] = [];
+    }
+    result[level].push(tree.val);
+    dst(tree.left, level + 1, result);
+    dst(tree.right, level + 1, result);
+  };
+
+  dst(root, 0, result);
 
   return result;
 };
